c48e57d0fe704555595ac1697289e0a51d7f4f15
Arunabha-Chatterjee <arunabha6289@gmail.com>
2022-11-01 02:50:05 -0400

    added

---
+++ DSA-with-C/array_c/insertation.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int size, i, new_element, index;
+    printf("Enter the size of the array : ");
+    scanf("%d",&size);
+    int array[size];
+    for ( i = 0; i < size; i++)
+    {
+        printf("\nEnter the %d index no element of the array : ",i);
+        scanf("%d",&array[i]);
+    }
+
+    printf("\n Elements have entered : ");
+    for ( i = 0; i < size; i++)
+    {
+        printf("%d ",array[i]);
+    }
+    printf("\n");
+
+    printf("\nEnter the element : ");
+    scanf("%d",&new_element);
+    printf("\nEnter index no want to insert : ");
+    scanf("%d",&index);
+    size++;
+    for (i=size; i>index; i--)
+    {
+        array[i]=array[i-1];
+    }
+    array[index]=new_element;
+    printf("\nArray elements after inserting a array : ");
+    for ( i = 0; i < size; i++)
+    {
+        printf("%d ",array[i]);
+    }
+
+}
